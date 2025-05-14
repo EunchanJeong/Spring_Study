22f54ee767cd5f57e303107fc493e6fd952cf4e8
Eunchan Jeong <jec65147@gmail.com>
2025-05-14 13:52:46 +0900

    first commit

---
+++ core/src/test/java/hello/core/beanfind/XmlAppContext.java
@@ -0,0 +1,12 @@
+package hello.core.beanfind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+    }
+}
